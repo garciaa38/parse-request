16e5487b6de698afdc85f699d7806446898d57b4
Max Fonda <mxwll155@gmail.com>
2023-12-06 19:19:24 -0500

    P1 done

--- server.js
+++ server.js
@@ -5,8 +5,15 @@ let server;
 /******************************************************************************/
 /******************* DO NOT CHANGE THE CODE ABOVE THIS LINE *******************/
 
-// Your code here 
+// Your code here
+const http = require("http");
 
+const PORT = 5000;
+
+server = http.createServer((req, res) => {
+});
+
+server.listen(PORT, () => console.log(`Server is listening on port ${PORT}`));
 /******************************************************************************/
 /******************* DO NOT CHANGE THE CODE BELOW THIS LINE *******************/
 
