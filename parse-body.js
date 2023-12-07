ac2d8c08bdafd75515539daf27d025739bee27d7
Max Fonda <mxwll155@gmail.com>
2023-12-06 20:21:55 -0500

    DONE

--- parse-body.js
+++ parse-body.js
@@ -1,26 +1,37 @@
 function firstStep(input) {
-  // Your code here 
+  return input.split('&');
 }
 
 function secondStep(input) {
-  // Your code here 
+  return input.map((keyValPair) => keyValPair.split('='))
 }
 
 function thirdStep(input) {
-  // Your code here 
+  return input.map(([key, value]) => [key, value.replace(/\+/g, " ")])
 }
 
 function fourthStep(input) {
-  // Your code here 
+  return input.map(([key, value]) => [key, decodeURIComponent(value)])
 }
 
 function fifthStep(input) {
-  // Your code here 
+  return input.reduce((acc, [key, value]) => {
+    acc[key] = value;
+    return acc;
+  }, {})
 }
 
 function parseBody(str) {
-  // Your code here 
+
+  let first = firstStep(str);
+  let second = secondStep(first)
+  let third = thirdStep(second);
+  let fourth = fourthStep(third)
+  return fifthStep(fourth);
 }
+
+let string = "username=azure+green&password=password%21"
+console.log(firstStep(string))
 
 /******************************************************************************/
 /******************* DO NOT CHANGE THE CODE BELOW THIS LINE *******************/
--- server.js
+++ server.js
@@ -6,11 +6,26 @@ let server;
 /******************* DO NOT CHANGE THE CODE ABOVE THIS LINE *******************/
 
 // Your code here
+
+
 const http = require("http");
-
 const PORT = 5000;
-
 server = http.createServer((req, res) => {
+
+    console.log(req.method, req.url);
+
+    let reqBody = '';
+    req.on('data', (data) => {
+        reqBody += data;
+    });
+
+    req.on('end', () => {
+        if (reqBody){
+            req.body = parseBody(reqBody);
+        }
+        sendFormPage(req, res);
+    });
+
 });
 
 server.listen(PORT, () => console.log(`Server is listening on port ${PORT}`));
